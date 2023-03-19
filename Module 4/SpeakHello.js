88a26d9327bf65ea8c53d06997e039d41ff6bebe
timkinnaird <127018536+timkinnaird@users.noreply.github.com>
2023-03-19 13:20:07 +0000

    update

--- Module 4/SpeakHello.js
+++ Module 4/SpeakHello.js
@@ -13,8 +13,8 @@ var speakWord = "Hello";
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function speak(name) {
-  console.log(speakWord + " " + helloSpeaker);
+  helloSpeaker.speak = function () {
+    console.log(speakWord + " " + helloSpeaker);
 }
 
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
